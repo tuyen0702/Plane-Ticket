da874d92b7f1e9db00cc1b920730b64321d079cb
PHƯỚC LÊ VĂN <16520959@ms.uit.edu.vn>
2018-04-22 17:22:37 +0700

    update report chapter 3

--- WIP/Source/PlaneTicketGUI/PlaneTicketGUI/Form1.Designer.cs
+++ WIP/Source/PlaneTicketGUI/PlaneTicketGUI/Form1.Designer.cs
@@ -519,7 +519,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(473, 498);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Nhập Liệu Thông Tin";
+            this.tabPage2.Text = "Xử Lý Thông Tin";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabControl4
